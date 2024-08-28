b66a0e89b03e2987b64b938018c29542d6e105db
Elena Mitrofanova <137034439+Elena-Mi@users.noreply.github.com>
2024-08-28 14:23:59 +0800

    Delete routes directory

--- routes/photo.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { photoByLink, upload } from '../controllers/photo.controller';
-const router = express.Router();
-
-router.post('/upload-by-link',photoByLink);
-router.post('/upload', upload);
-
-export default router;
